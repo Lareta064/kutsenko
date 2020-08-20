348617cc63d92f7b4ff99a55741ae5148f5fe602
Lareta <lareta064@gmail.com>
2020-08-20 10:55:00 +0300

    модалки

--- src/js/main.js
+++ src/js/main.js
@@ -22,5 +22,104 @@ $(document).ready(function () {
 			item.classList.add('active');
 		}
 	});
+	// показать/скрыть весь Модуль
+	const modulBtn = document.querySelector('.btn-allModul');
+	const saleInfo = document.querySelector('#saleInfo');
+	const modulBlock = document.querySelector('.modul-block__hidden');
+	const btnCloseModul = modulBlock.querySelector('.btn-closeModul');
 
+	modulBtn.addEventListener('click', function () {
+		// console.log('555');
+		this.classList.add('hide');
+		modulBlock.classList.add('active');
+		saleInfo.classList.add('active');
+
+	});
+
+	if (btnCloseModul) {
+		btnCloseModul.addEventListener('click', function () {
+
+			modulBtn.classList.remove('hide');
+			modulBlock.classList.remove('active');
+			saleInfo.classList.remove('active');
+
+		});
+	}
+
+	// MODAL Клик по ссылке "Закрыть".
+	// $('.popup-close').click(function () {
+	// 	$(this).parents('.popup-fade').fadeOut();
+	// 	return false;
+	// });
+
+	// // Закрытие по клавише Esc.
+	// $(document).keydown(function (e) {
+	// 	if (e.keyCode === 27) {
+	// 		e.stopPropagation();
+	// 		$('.popup-fade-1').fadeOut();
+	// 	}
+	// });
+
+	// // Клик по фону, но не по окну.
+	// let bodyEl = $('body')
+	// $('.popup-fade-1').click(function (e) {
+	// 	if ($(e.target).closest('.popup').length == 0) {
+	// 		$(this).fadeOut();
+	// 		bodyEl.removeClass('noscroll');
+	// 	}
+	// });
+
+	$('.popup-open1').click(function () {
+		$('.popup-fade-1').fadeIn();
+		bodyEl.addClass('noscroll');
+		return false;
+	});
+
+	$('.popup-close').click(function () {
+		$(this).parents('.popup-fade-1').fadeOut();
+		bodyEl.removeClass('noscroll');
+		return false;
+	});
+
+	$(document).keydown(function (e) {
+		if (e.keyCode === 27) {
+			e.stopPropagation();
+			$('.popup-fade-1').fadeOut();
+			bodyEl.removeClass('noscroll');
+		}
+	});
+
+	$('.popup-fade-1').click(function (e) {
+		if ($(e.target).closest('.popup').length == 0) {
+			$(this).fadeOut();
+			bodyEl.removeClass('noscroll');
+		}
+	});
+	// модал-2
+	$('.popup-open2').click(function () {
+		$('.popup-fade-2').fadeIn();
+		bodyEl.addClass('noscroll');
+		return false;
+	});
+
+	$('.popup-close').click(function () {
+		$(this).parents('.popup-fade-2').fadeOut();
+		bodyEl.removeClass('noscroll');
+		return false;
+	});
+
+	$(document).keydown(function (e) {
+		if (e.keyCode === 27) {
+			e.stopPropagation();
+			$('.popup-fade-2').fadeOut();
+			bodyEl.removeClass('noscroll');
+		}
+	});
+
+	$('.popup-fade-2').click(function (e) {
+		if ($(e.target).closest('.popup').length == 0) {
+			$(this).fadeOut();
+			bodyEl.removeClass('noscroll');
+		}
+	});
 })
